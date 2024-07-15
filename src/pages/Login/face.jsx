be6ef2e43775c2daefaf85db5ad268825b705a56
Raja Simon <rajasimonio@gmail.com>
2024-07-15 23:31:08 +0530

    Update face.jsx

--- src/pages/Login/face.jsx
+++ src/pages/Login/face.jsx
@@ -81,7 +81,7 @@ export const Face = ({ setMode, setName, typeInput }) => {
 
             const splittedEmail = email.split("@")
             const identifyName = splittedEmail[0]
-
+            console.log(identifyName, splittedEmail);
             if (identifyName === typeInput) {
               // success
               login(token, typeInput, name)
