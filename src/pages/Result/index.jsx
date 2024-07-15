ee27f746313cc7cce400c1bacd613b095f607f23
Raja Simon <rajasimonio@gmail.com>
2024-07-15 23:50:33 +0530

    Update index.jsx

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -97,8 +97,8 @@ const Result = () => {
     let tabCount = 0
     let faceCount = 0
     let totalCount = 0
-    if (autoProctorReport.reportData.evidences.length > 0) {
-      autoProctorReport.reportData.evidences.forEach(evidenceReport => {
+    if (autoProctorReport.reportData.evidence.length > 0) {
+      autoProctorReport.reportData.evidence.forEach(evidenceReport => {
         
         if (!Array.isArray(evidenceReport)) {
           console.log(evidenceReport)
